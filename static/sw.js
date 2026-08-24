55b5a560272c5ab92c74d84f483532859c1d41d7
yosephseleshi08 <yosephseleshi08@gmail.com>
2026-08-24 23:15:59 +0300

    Add files via upload

--- static/sw.js
+++ static/sw.js
@@ -1,1 +1,44 @@
+const CACHE_NAME = 'restaurant-cms-v2';
+const urlsToCache = [
+  '/',
+  '/menu',
+  '/about',
+  '/contact',
+  '/reservations',
+  '/order'
+];
 
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
